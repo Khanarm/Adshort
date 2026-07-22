e83c12a78ff2d5ab38c05bb0ab9901a09130b229
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-22 17:18:05 +0530

    Update generate.py

--- routes/generate.py
+++ routes/generate.py
@@ -1,1 +1,8 @@
+from flask import Blueprint, render_template
 
+generate_bp = Blueprint("generate", __name__)
+
+
+@generate_bp.route("/generate")
+def generate():
+    return render_template("generate.html")
