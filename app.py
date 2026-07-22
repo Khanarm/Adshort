f4130aa2f0d2e29bed01d5de5475ddc534c75fcf
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-22 17:10:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
 from config import Config
 
+# Blueprints
+from routes.auth import auth_bp
+
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Register Blueprints
+app.register_blueprint(auth_bp)
 
 
 @app.route("/")
