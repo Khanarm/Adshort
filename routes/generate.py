f30690a94a2e8119785c9b39efb4568922d259d5
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-22 17:59:41 +0530

    Update generate.py

--- routes/generate.py
+++ routes/generate.py
@@ -1,8 +1,28 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, session, redirect
 
 generate_bp = Blueprint("generate", __name__)
 
 
-@generate_bp.route("/generate")
+@generate_bp.route("/generate", methods=["GET", "POST"])
 def generate():
-    return render_template("generate.html")
+
+    if "user_id" not in session:
+        return redirect("/login")
+
+    plan = request.args.get("plan", 1)
+
+    if request.method == "POST":
+
+        # Future
+        # Platform
+        # Platform Name
+        # Destination URL
+        # Short Link Generate
+        # MongoDB Save
+
+        pass
+
+    return render_template(
+        "generate.html",
+        plan=plan
+    )
