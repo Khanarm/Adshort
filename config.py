99e2a4bb8727f8c62ee8a2fe668f74dd5c525332
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-23 10:19:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
+import os
+
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "adshort")
+
     MONGO_URI = os.getenv(
         "MONGO_URI",
-        "mongodb+srv://Bikash:Bikash@bikash.yl2nhcy.mongodb.net/unlock2earn?retryWrites=true&w=majority"
+        "mongodb+srv://Bikash:Bikash@bikash.yl2nhcy.mongodb.net/?retryWrites=true&w=majority"
     )
+
+    BASE_URL = os.getenv("BASE_URL", "http://localhost:5000")
