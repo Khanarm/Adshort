d19a0abcd4e9627e9b4680cee55590b978880b7d
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-23 10:10:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,8 @@ load_dotenv()
 
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY, "adshort")
-    MONGO_URI = os.getenv("MONGO_URI, "mongodb+srv://Bikash:Bikash@bikash.yl2nhcy.mongodb.net/?retryWrites=true&w=majority")
+    SECRET_KEY = os.getenv("SECRET_KEY", "adshort")
+    MONGO_URI = os.getenv(
+        "MONGO_URI",
+        "mongodb+srv://Bikash:Bikash@bikash.yl2nhcy.mongodb.net/?retryWrites=true&w=majority"
+    )
