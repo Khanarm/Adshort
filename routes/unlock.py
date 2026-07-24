2adafdef23f12c9db50e21fdd23a89ef37d83392
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-24 17:15:24 +0530

    Update unlock.py

--- routes/unlock.py
+++ routes/unlock.py
@@ -1,8 +1,26 @@
 from flask import Blueprint, render_template
+from models.links import links
 
 unlock_bp = Blueprint("unlock", __name__)
 
 
-@unlock_bp.route("/unlock")
-def unlock():
-    return render_template("unlock.html")
+@unlock_bp.route("/<code>")
+def unlock(code):
+
+    # Database se link search karo
+    link = links.find_one({"code": code})
+
+    # Agar link nahi mila
+    if not link:
+        return render_template("404.html"), 404
+
+    # Unlock page show karo
+    return render_template(
+        "unlock.html",
+        code=link["code"],
+        platform=link["platform"],
+        action_name=link["action_name"],
+        ads=link["ads"],
+        cpm=link["cpm"],
+        destination_url=link["destination_url"]
+    )
