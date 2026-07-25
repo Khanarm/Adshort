5bf8ce403153243de79bf8d103aea5d15da58c12
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-25 21:28:35 +0530

    Create go.py

---
+++ routes/go.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, redirect
+
+from models.links import links
+
+go_bp = Blueprint("go", __name__)
+
+
+@go_bp.route("/go/<track_code>")
+def go(track_code):
+
+    link = links.find_one({"track_code": track_code})
+
+    if not link:
+        return "Invalid Link"
+
+    links.update_one(
+        {"_id": link["_id"]},
+        {
+            "$inc": {
+                "clicks": 1,
+                "visitors": 1
+            }
+        }
+    )
+
+    return redirect(link["destination_url"])
