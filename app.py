2717d3f53cedf47046d1951d8129145138a7f4ef
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-25 18:21:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from routes.admin.login import admin_login_bp
 from routes.admin.dashboard import admin_dashboard_bp
 from routes.admin.users import admin_users_bp
 from routes.admin.withdraw import admin_withdraw_bp
+from routes.admin.links import admin_links_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -35,6 +36,7 @@ app.register_blueprint(admin_login_bp)
 app.register_blueprint(admin_dashboard_bp)
 app.register_blueprint(admin_users_bp)
 app.register_blueprint(admin_withdraw_bp)
+app.register_blueprint(admin_links_bp)
 
 @app.route("/")
 def home():
