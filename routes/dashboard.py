1a30488bd011e758503d2eb80c03a4779cf34b9b
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-25 21:49:32 +0530

    Update dashboard.py

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -8,16 +8,15 @@ dashboard_bp = Blueprint("dashboard", __name__)
 @dashboard_bp.route("/dashboard")
 def dashboard():
 
-    # User not logged in
     if "user_id" not in session:
         return redirect("/login")
 
     user = users.find_one({
-    "_id": ObjectId(session["user_id"])
-})
+        "_id": ObjectId(session["user_id"])
+    })
 
-return render_template(
-    "dashboard.html",
-    username=session["username"],
-    user=user
-)
+    return render_template(
+        "dashboard.html",
+        username=session["username"],
+        user=user
+    )
