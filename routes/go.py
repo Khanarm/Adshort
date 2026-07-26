733eec912e3c0799ca24b04cc5de340f565d9796
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-26 17:43:26 +0530

    Update go.py

--- routes/go.py
+++ routes/go.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, redirect
+from bson import ObjectId
 
 from models.links import links
+from models.users import users
+from utils.referral import give_referral_commission
 
 go_bp = Blueprint("go", __name__)
 
@@ -15,6 +18,7 @@ def go(track_code):
 
     earning = link["cpm"] / 1000
 
+    # Link stats
     links.update_one(
         {"_id": link["_id"]},
         {
@@ -25,5 +29,18 @@ def go(track_code):
             }
         }
     )
+
+    # Owner balance
+    users.update_one(
+        {"_id": ObjectId(link["user_id"])},
+        {
+            "$inc": {
+                "current_balance": earning
+            }
+        }
+    )
+
+    # 5% Referral Commission
+    give_referral_commission(link["user_id"], earning)
 
     return redirect(link["destination_url"])
