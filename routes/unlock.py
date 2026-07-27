8ac0279c150ee9f67b128cdb67242d0aca24ff2a
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-27 19:27:51 +0530

    Update unlock.py

--- routes/unlock.py
+++ routes/unlock.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 from models.links import links
+from models.unlock_sessions import unlock_sessions
+import uuid
 
 unlock_bp = Blueprint("unlock", __name__)
 
@@ -7,14 +9,24 @@ unlock_bp = Blueprint("unlock", __name__)
 @unlock_bp.route("/<code>")
 def unlock(code):
 
-    # Database se link search karo
     link = links.find_one({"code": code})
 
-    # Agar link nahi mila
     if not link:
         return render_template("404.html"), 404
 
-    # Unlock page show karo
+    # Guest visitor id
+    visitor_id = session.get("visitor_id")
+
+    if not visitor_id:
+        visitor_id = str(uuid.uuid4())
+        session["visitor_id"] = visitor_id
+
+    # Purani session delete
+    unlock_sessions.delete_many({
+        "visitor_id": visitor_id,
+        "code": code
+    })
+
     return render_template(
         "unlock.html",
         code=link["code"],
