246a61034ef2f73ad759c38a8da372b037bdad4f
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-27 17:44:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "adshort")
@@ -9,3 +10,7 @@ class Config:
     )
 
     BASE_URL = os.getenv("BASE_URL", "http://localhost:5000")
+
+    # 👇 Add these
+    SESSION_PERMANENT = True
+    PERMANENT_SESSION_LIFETIME = timedelta(days=30)
