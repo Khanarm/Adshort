c36b2e270861bb777f56882734649becb2e57aea
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-28 18:54:47 +0530

    Add imports and initialize report blueprint

--- routes/report.py
+++ routes/report.py
@@ -1,1 +1,6 @@
+from flask import Blueprint, render_template, request, session, redirect
+from bson import ObjectId
+from datetime import datetime
+from mongo import db
 
+report_bp = Blueprint("report", __name__)
