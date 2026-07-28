45ed5f9983c9c483f316af690ddc669c311ad8ac
Khanarm <142902201+Khanarm@users.noreply.github.com>
2026-07-28 19:42:49 +0530

    Update history.py

--- routes/history.py
+++ routes/history.py
@@ -1,5 +1,22 @@
 from flask import Blueprint, render_template, session, redirect
-from bson import ObjectId
-from mongo import db
+from models.withdrawals import withdrawals
 
 history_bp = Blueprint("history", __name__)
+
+
+@history_bp.route("/withdraw-history")
+def withdraw_history():
+
+    if "user_id" not in session:
+        return redirect("/login")
+
+    history = list(
+        withdrawals.find(
+            {"user_id": session["user_id"]}
+        ).sort("created_at", -1)
+    )
+
+    return render_template(
+        "history.html",
+        history=history
+    )
